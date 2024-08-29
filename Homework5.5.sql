0f130a2353dbdeae7176d4dbd27adb29e5a5ddea
Burak Kırıcı <burakpinar76@gmail.com>
2024-08-29 11:17:04 +0300

    Add files via upload

---
+++ Homework5.5.sql
@@ -0,0 +1,24 @@
+
+-- Homework 6
+
+-- Question 1
+--SELECT AVG(rental_rate) FROM film;
+
+-- Question 2
+
+--SELECT COUNT(*) FROM film
+--WHERE title LIKE 'C%';
+
+-- Question 3
+
+--SELECT MAX(length) FROM film
+--WHERE rental_rate = 0.99;
+
+-- Question 4
+--SELECT COUNT(DISTINCT replacement_cost) FROM film
+--WHERE length > 150;
+
+------------------------------------------------------------------
+
+
+
